8122c05777d02e2b5add90c7850f5b4f432ab66d
moqimoqidea <moqimoqidea@gmail.com>
2022-11-02 10:29:04 +0800

    Update gin_demo.go

--- pkg/gin/gin_demo.go
+++ pkg/gin/gin_demo.go
@@ -27,8 +27,19 @@ func main() {
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
+	router.GET("/someJson", someJson)
 
 	_ = router.Run("localhost:8080")
+}
+
+func someJson(c *gin.Context) {
+	data := map[string]interface{}{
+		"lang": "GO语言",
+		"tag":  "<br>",
+	}
+
+	// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+	c.AsciiJSON(http.StatusOK, data)
 }
 
 // getAlbums responds with the list of all albums as JSON.

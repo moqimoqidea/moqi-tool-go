b5b213d0bbb31135416f2e1f6932d5117b8ea971
moqimoqidea <moqimoqidea@gmail.com>
2023-01-05 14:17:41 +0800

    update

--- pkg/sjson/sjson_test.go
+++ pkg/sjson/sjson_test.go
@@ -5,6 +5,7 @@
 package sjson
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/tidwall/sjson"
 	"testing"
@@ -16,5 +17,17 @@ const (
 
 func TestSjson1(t *testing.T) {
 	value, _ := sjson.Set(simpleJson, "name.last", "Anderson")
+	fmt.Println("value -> ", value)
+}
+
+func TestSjson2(t *testing.T) {
+	tempMap := make(map[string]string)
+	tempMap["k1"] = "v1"
+	tempMap["k2"] = "v2"
+	tempMap["k3"] = "v3"
+
+	jsonStr, _ := json.Marshal(tempMap)
+	fmt.Println("jsonStr -> ", jsonStr)
+	value, _ := sjson.SetRaw(simpleJson, "mapData", string(jsonStr))
 	fmt.Println("value -> ", value)
 }

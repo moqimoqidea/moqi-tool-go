f396492bdc0696ad43ec984a0d0bccc864b18cd8
moqimoqidea <moqimoqidea@gmail.com>
2023-01-13 11:16:31 +0800

    update

---
+++ pkg/progressive/range_test.go
@@ -0,0 +1,20 @@
+package progressive
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRange1(t *testing.T) {
+	var m = [...]int{1, 2, 3, 4, 5}
+
+	for i, v := range m {
+		go func() {
+			time.Sleep(time.Second * 3)
+			fmt.Println(i, v)
+		}()
+	}
+
+	time.Sleep(time.Second * 10)
+}

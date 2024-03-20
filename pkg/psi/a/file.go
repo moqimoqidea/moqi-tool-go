ed15fedbc3359c1e401fdc336cd0333b5732b555
moqimoqidea <moqimoqidea@gmail.com>
2024-03-20 23:20:54 +0800

    Add psi test files +1.

--- pkg/psi/a/file.go
+++ pkg/psi/a/file.go
@@ -1,6 +1,21 @@
 package a
 
-import "github.com/moqimoqidea/moqi-tool-go/pkg/psi/b"
+import (
+	"fmt"
+	"github.com/moqimoqidea/moqi-tool-go/pkg/psi/b"
+)
+
+// MyGreeter 结构体类型
+type MyGreeter struct{}
+
+// Hello 方法实现了 b 包中 Greeter 接口的 Hello 方法
+func (mg MyGreeter) Hello() string {
+	return "Hello from package a"
+}
+
+func (mg MyGreeter) HelloWithParameter(a string) string {
+	return "Hello from package a with parameter: " + a
+}
 
 func main() {
 	b.MethodHello()
@@ -9,4 +24,10 @@ func main() {
 	myStruct.FunctionHello()
 
 	myStruct.FunctionHelloXXXYYYMMM()
+
+	mg := MyGreeter{}
+	greeting := b.Greet(mg) // 使用 b 包的 Greet 函数
+	fmt.Println(greeting)
+	greetingWithParameter := b.GreetWithParameter(mg, "moqi") // 使用 b 包的 GreetWithParameter 函数
+	fmt.Println(greetingWithParameter)
 }
--- pkg/psi/b/file.go
+++ pkg/psi/b/file.go
@@ -2,6 +2,21 @@ package b
 
 import "fmt"
 
+// Greeter 接口定义了一个方法签名 Hello
+type Greeter interface {
+	Hello() string
+	HelloWithParameter(a string) string
+}
+
+// Greet 函数接受实现了 Greeter 接口的任何类型的实例
+func Greet(g Greeter) string {
+	return g.Hello()
+}
+
+func GreetWithParameter(g Greeter, a string) string {
+	return g.HelloWithParameter(a)
+}
+
 type MyStruct struct {
 	Name string
 }

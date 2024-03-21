8212c7c4a021e3465a42e112759108f8a844cb43
moqimoqidea <moqimoqidea@gmail.com>
2024-03-21 12:33:51 +0800

    test +1

--- pkg/psi/b/file2.go
+++ pkg/psi/b/file2.go
@@ -5,3 +5,7 @@ import "fmt"
 func (m MyStruct) FunctionHelloXXX() {
 	fmt.Println("FunctionHello from b function xxx.")
 }
+
+func (m MyStruct) functionInside() {
+	fmt.Println("functionInside from b MyStruct.")
+}
